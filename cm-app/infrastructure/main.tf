001d7377c02b32701602da1cad0b44885022b02a
rfctf <67594801+rfctf@users.noreply.github.com>
2025-07-11 15:21:16 +0800

    Update main.tf

--- cm-app/infrastructure/main.tf
+++ cm-app/infrastructure/main.tf
@@ -1,8 +1,2 @@
 # TODO: paste your Terraform (or CloudFormation) to stand up VPC, EC2, EKS, S3
-resource "aws_security_group" "demo_sg" {
-  name        = "demo-open-ssh"
-  description = "Demo SG for testing AWS006"
-  vpc_id      = var.vpc_id   
 
-\
-}
